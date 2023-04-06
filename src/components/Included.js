daba17123eea8249ca48a23baf071956fd34e81b
Matthew Banh <mattbanh92@gmail.com>
2023-04-06 00:02:36 -0700

    completed what's included component

--- src/components/Included.js
+++ src/components/Included.js
@@ -26,28 +26,26 @@ export default function Included() {
             What's Included
           </Heading>
         </article>
-        <div className="mx-auto md:max-w-[590px] lg:max-w-[1120px] ">
-          <div className="mb-9">
-            <div className="md:max-w-[590px]">
-              <Heading as="h3" size="sectionSub" className="mb-6">
-                As part of
-                <span className="text-academy-gold"> course registration </span>
-                , you will receive the following
-              </Heading>
-            </div>
+        <div className="mx-auto max-w-[560px] xl:max-w-[1120px] ">
+          <div className="md:max-w-[560px] mb-9">
+            <Heading as="h3" size="sectionSub" className="mb-6">
+              As part of
+              <span className="text-academy-gold"> course registration </span>,
+              you will receive the following
+            </Heading>
           </div>
-          <section className="flex flex-col lg:flex-row">
+          <section className="flex flex-col gap-10 xl:flex-row xl:gap-0">
             <div
-              className="relative xl:w-1/2 flex justify-end py-6 before:absolute before: before:border-academy-green before:border-l-[3px]
-                after:absolute after:bottom-0 after:left-0 after:w-1/2 after:border-b-[3px] after:border-academy-green"
+              className="flex justify-end relative py-6 pl-6 xl:w-1/2 before:absolute before:left-0 before:top-0 before:h-full before:w-4/5 before:border-academy-green before:border-t-[3px] before:border-l-[3px]
+                after:absolute after:bottom-0 after:left-0 after:w-1/5 after:border-b-[3px] after:border-academy-green"
             >
-              <Image src={included} alt="" />
+              <Image src={included} alt="" className="object-contain" />
             </div>
-            <article className="md:mx-auto py-8 pr-6 md:pr-0 border border-academy-gold md:w-[610px] xl:w-1/2">
-              <ul className="list-['—'] mb-8 ml-4 p-4">
+            <article className="md:mx-auto p-12 xl:w-1/2 border border-academy-gold ">
+              <ul className="list-['—'] mb-12 ml-4 flex flex-col gap-6 md:gap-8">
                 {includedItems.map((item, i) => (
-                  <li key={i} className="text-academy-gold pl-5 mb-4">
-                    <Text size="list" color="primary">
+                  <li key={i} className="text-academy-gold pl-5">
+                    <Text size="list" color="primary" family="secondary">
                       {item}
                     </Text>
                   </li>
